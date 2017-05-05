d6d6e5ca65de64699bc8f7da55919bb1ae6c4422
Phuc Nguyen <ph.nguyen@linkbynet.com>
2017-05-05 11:42:42 +0700

    update evn variable

--- files/db.php
+++ files/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => sprintf("mysql:host=%s;dbname=%s", $_ENV["DB_HOST"], $_ENV["DB_NAME"]),
+    'dsn' => sprintf("%s:host=%s;dbname=%s", $_ENV["DB_TYPE"], $_ENV["DB_HOST"], $_ENV["DB_NAME"]),
     'username' => $_ENV["DB_USER"],
     'password' => $_ENV["DB_PASS"],
     'charset' => 'utf8',
