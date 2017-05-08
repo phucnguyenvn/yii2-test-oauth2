ac2bba7f3cace587e9667a49cd12644f67730804
Phuc Nguyen <ph.nguyen@linkbynet.com>
2017-05-08 11:05:02 +0700

    update chmod for test folder

--- files/start.sh
+++ files/start.sh
@@ -8,6 +8,7 @@ su www-data -s /bin/bash -c 'php yii migrate/up --migrationPath=@vendor/macfly/y
 su www-data -s /bin/bash -c '[ -d migrations ] && php yii migrate/up --interactive=0'
 su www-data -s /bin/bash -c 'ls'
 su www-data -s /bin/bash -c 'sleep 30'
-su www-data -s /bin/bash -c 'curl -v http://127.0.0.1:8080'
+/wait-for-it.sh 127.0.0.1:80
+su www-data -s /bin/bash -c 'curl -v http://127.0.0.1:80'
 exec "apache2-foreground"
 # su www-data -s /bin/bash -c './vendor/bin/codecept run'
