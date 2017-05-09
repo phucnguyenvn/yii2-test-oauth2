58c429ba2b0ea4d31b3d537d4234367c303f8848
Phuc Nguyen <ph.nguyen@linkbynet.com>
2017-05-09 13:41:02 +0700

    update chmod for test folder

--- files/run.sh
+++ files/run.sh
@@ -2,5 +2,6 @@
 service apache2 restart
 /wait-for-it.sh db:3306
 /wait-for-it.sh 127.0.0.1:80
-php yii migrate/up --migrationPath=@vendor/macfly/yii2-oauth2-server/src/migrations --interactive=0
+# php yii migrate/up --migrationPath=@vendor/macfly/yii2-oauth2-server/src/migrations --interactive=0
+php yii migrate/up --migrationPath=@app --interactive=0
 ./vendor/bin/codecept run --debug
